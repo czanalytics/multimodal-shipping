2502fbf8e74c55cbcc82ef1553561e26bcfcf32b
czanalytics <aatos.heikkinen@czanalytics.com>
2023-06-02 14:33:03 +0300

    drating lane library with colab.py configuration

---
+++ lane/conf/colab.py
@@ -0,0 +1,13 @@
+
+def hello():
+    print('Hello from Lane :')
+
+def conf2():
+    """Default Lane configation"""
+    lt = {'a':1, 'ab':'xxx'}
+    return lt
+
+def foo2(x):
+    return (x,x*2)
+
+
